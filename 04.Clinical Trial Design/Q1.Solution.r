dea69c1e8a069b764d59c0c4350c7765b2b13f9f
Chunhua Yu <48463063+chunhuayu@users.noreply.github.com>
2019-05-30 00:45:24 -0400

    Create Q1.Solution.r

---
+++ 04.Clinical Trial Design/Q1.Solution.r
@@ -0,0 +1,11 @@
+### Q1-i
+# NA=NB=300/2=150
+### Q1-ii
+> library(randPack)
+> male.African=blockrand(n=50, id.prefix='M', stratum='African male')
+> male.White=blockrand(n=50, id.prefix='M', stratum='White male')
+> male.Asian=blockrand(n=50, id.prefix='M', stratum='Asian male')
+> female.African=blockrand(n=50, id.prefix='F', stratum='African female')
+> female.White=blockrand(n=50, id.prefix='F', stratum='White female')
+> female.Asian=blockrand(n=50, id.prefix='F', stratum='Asian female')
+# Female.African.TreatmentA=25
